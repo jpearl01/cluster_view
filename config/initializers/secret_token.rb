ad50fdc678befc0e14baa29d8707491897b364b7
Josh Earl <joshearl1@hotmail.com>
2014-07-11 15:52:19 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClusterView::Application.config.secret_key_base = 'f95874a4d6fe23174020a3fe6a6a9ad9252f11536a8c902d6a5f6fa79b2679e30f8300518dea21e1a4d0fb2c2213173c74d2e06e402464968f263970a7939d64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ClusterView::Application.config.session_store :cookie_store, key: '_cluster_view_session'
