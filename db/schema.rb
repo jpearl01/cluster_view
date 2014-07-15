90a6912db7833fbec23c88030200c3b116bcb3f5
Josh Earl <joshearl1@hotmail.com>
2014-07-15 18:39:01 -0400

    Added in some associations between the user and the projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715170501) do
+ActiveRecord::Schema.define(version: 20140715223158) do
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
